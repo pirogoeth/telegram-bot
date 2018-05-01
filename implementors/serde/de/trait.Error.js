f67315c081b821a6df4eedd3c91790175e0cd04c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-05-01 11:35:05 +0000

    rebuild pages at 2698bc0

--- implementors/serde/de/trait.Error.js
+++ implementors/serde/de/trait.Error.js
@@ -1,6 +1,7 @@
 (function() {var implementors = {};
 implementors["serde"] = [];
 implementors["serde_json"] = ["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>",];
+implementors["serde_value"] = ["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"serde_value/enum.DeserializerError.html\" title=\"enum serde_value::DeserializerError\">DeserializerError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- telegram_bot_raw/types/update/sidebar-items.js
+++ telegram_bot_raw/types/update/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["UpdateKind","Kind of the incoming update."]],"struct":[["RawUpdate","This object represents an incoming update. Directly mapped."],["Update","This object represents an incoming update."]]});
+initSidebarItems({"enum":[["UpdateKind","Kind of the incoming update."]],"struct":[["Update","This object represents an incoming update."]]});
